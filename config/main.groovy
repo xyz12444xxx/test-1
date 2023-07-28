217fac1643878d1c2976a6be8d3929bffc86aec0
Kathim Anangoor-EXTERNAL Shereif <S0134951@gemalto.com>
2023-07-28 12:36:06 +0800

    update: add maven

--- config/main.groovy
+++ config/main.groovy
@@ -1,9 +1,16 @@
 pipeline {
     agent any
+    tools {
+        maven 'Maven 3.6.3'
+    }
     stages {
         stage('Build') {
             steps {
                 echo 'Building..'
+                // check if mvn is installed
+                sh 'mvn --version'
+                // if not, then install
+                sh 'mvn clean install'
             }
         }
         stage('Test') {
