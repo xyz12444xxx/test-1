16b78c753a1eefa7954cbf9423092dc050ab02e0
Kathim Anangoor-EXTERNAL Shereif <S0134951@gemalto.com>
2023-07-31 13:24:45 +0800

    update main.py

--- helpers/my-bash-scripts.sh
+++ helpers/my-bash-scripts.sh
@@ -5,3 +5,6 @@ gacp() {
     git commit -m "$1"
     git push
 }
+
+# add the following line to your .bashrc or .bash_profile
+# source ~/path/to/test1/helpers/my-bash-scripts.sh
--- src/main.py
+++ src/main.py
@@ -1,14 +1,19 @@
 import datetime
-
+import os
 
 print("hello thales!")
 
 # create a text file with some data inside
 def create_file():
     # open or create a file under target folder
+    path = "target"
+    # create directory if does not exist
+    if not os.path.exists(path):
+        os.makedirs(path)                
+    
     # name convention is ddmmyyyy_hhmmss.log
     fileName = datetime.datetime.now().strftime("%d%m%Y_%H%M%S") + ".log"
-    f = open("target/" + fileName, "w+")
+    f = open(path + "/" + fileName, "w+")
     for i in range(10):
         f.write("This is line %d\n" % (i+1))
     f.close()
