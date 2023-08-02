38fc73e779182aedf81075c0342b64b40d6401f0
Kathim Anangoor-EXTERNAL Shereif <S0134951@gemalto.com>
2023-08-02 14:32:05 +0800

    fix: import issue -25

--- config/main.groovy
+++ config/main.groovy
@@ -6,7 +6,7 @@ pipeline {
         string (
             name: 'artifactory_server_url',
             description: 'Artifactory server url',
-            defaultValue: 'http://"172.21.0.2:8082/artifactory/demo-work/',
+            defaultValue: 'http://172.21.0.2:8082/artifactory/demo-work/',
             required: true
         )
         credentials (
