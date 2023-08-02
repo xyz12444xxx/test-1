852705e9fe8df03114e22c557ea4dbae4ebd2e87
Kathim Anangoor-EXTERNAL Shereif <S0134951@gemalto.com>
2023-08-02 15:13:24 +0800

    fix: import issue -38

--- config/main.groovy
+++ config/main.groovy
@@ -68,7 +68,7 @@ pipeline {
 void initiate() {
     // create artifactory server    
     try {
-        jfrog.init('artifactory-1', params.artifactory_server_url, params.artifactory_cred_id, 'logs')
+        // jfrog.init('artifactory-1', params.artifactory_server_url, params.artifactory_cred_id, 'logs')
     } catch (Exception e) {
         echo "Failed to create Artifactory server-echo ${e}"
         error "Failed to create Artifactory server"
