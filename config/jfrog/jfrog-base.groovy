45c9bdea92bd5b81afa7e983b65f6cc7bfe6802f
Kathim Anangoor-EXTERNAL Shereif <S0134951@gemalto.com>
2023-08-08 17:35:13 +0800

    test-113

--- config/jfrog/jfrog-base.groovy
+++ config/jfrog/jfrog-base.groovy
@@ -23,12 +23,10 @@ def init(String id, String serverUrl, String repo, String credentialsId, String 
 }
 
 def uploadReports(String fromDir, String[] filenames) {
-    node {
-        withCredentials([usernamePassword(credentialsId: credentialsId, passwordVariable: 'PASSWORD', usernameVariable: 'USERNAME')]) {
-            for (String filename : filenames) {
-                def result = sh(script: "curl -k -T ${fromDir}/${filename} ${this.serverUrl}/artifactory/${this.repo}/${this.reportsStorePath}/${filename} -u " + '$USERNAME:$PASSWORD', returnStdout: true).trim()
-                echo "${result}"
-            }
+    withCredentials([usernamePassword(credentialsId: credentialsId, passwordVariable: 'PASSWORD', usernameVariable: 'USERNAME')]) {
+        for (String filename : filenames) {
+            def result = sh(script: "curl -k -T ${fromDir}/${filename} ${this.serverUrl}/artifactory/${this.repo}/${this.reportsStorePath}/${filename} -u " + '$USERNAME:$PASSWORD', returnStdout: true).trim()
+            echo "${result}"
         }
     }
 }
