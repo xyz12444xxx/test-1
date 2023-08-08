6d5114449e90833e395f0983c587c5ae5091e57b
Kathim Anangoor-EXTERNAL Shereif <S0134951@gemalto.com>
2023-08-08 16:58:20 +0800

    test-105

--- config/jfrog/jfrog-base.groovy
+++ config/jfrog/jfrog-base.groovy
@@ -5,7 +5,7 @@ def init(String id, String serverUrl, String repo, String credentialsId, String 
         // with credentials to get username and password from the jenkins credentials store
         withCredentials([usernamePassword(credentialsId: credentialsId, passwordVariable: 'password', usernameVariable: 'username')]) {
             // call curl to check if server is up in a shell script
-            def serverUp = sh(script: "curl -k ${serverUrl}/api/system/ping -u ${username}:${password}", returnStdout: true).trim()
+            def serverUp = sh(script: "curl -k ${serverUrl}/api/system/ping -u $username:$password", returnStdout: true).trim()
             // do not show password in the variable
             serverUp = serverUp.replaceAll("${password}", "********")
             echo "server: ${serverUp}"
