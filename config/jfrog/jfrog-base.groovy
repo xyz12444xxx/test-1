ce21109685c4b8917547d54d402893ba66353a57
Kathim Anangoor-EXTERNAL Shereif <S0134951@gemalto.com>
2023-08-08 16:07:30 +0800

    test-100

--- config/jfrog/jfrog-base.groovy
+++ config/jfrog/jfrog-base.groovy
@@ -2,30 +2,23 @@ _instance = null
 
 def init(String id, String serverUrl, String repo, String credentialsId, String reportsStorePath) {
     if (!_instance) {
-        _instance = new JfrogBase(id, serverUrl, repo, credentialsId, reportsStorePath, 
-        Artifactory.newServer(url: serverUrl, credentialsId: credentialsId))
-
-        sh "curl ${serverUrl}/api/system/ping"
-    } else {
-        echo 'instance already exists'
-    }
+        _instance = new JfrogBase(id, serverUrl, repo, credentialsId, reportsStorePath)
 }
 
-def uploadReports(String fromDir, String[] filenames) {
-    if (!_instance) {
-        throw new Exception("JfrogBase instance not initialized")
+def uploadReports(String fromDir, String[] filenames) {    
+    // use REST API from artifactory to check if server is up
+    def url = new URL(serverUrl + 'api/system/ping')
+    def connection = url.openConnection()
+    withCredentials([usernamePassword(credentialsId: credentialsId, usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')]) {
+        connection.setRequestProperty("Authorization", "Basic " + "${USERNAME}:${PASSWORD}".bytes.encodeBase64().toString())
     }
 
-    for (filename in filenames) {
-        _instance.server.upload spec: """{
-            "files": [
-                {
-                    "pattern": "${fromDir}/${filename}",
-                    "target": "${_instance.repo}/${_instance.reportsStorePath}/"
-                }
-            ]
-        }"""
+    connection.connect()
+    if (connection.responseCode != 200) {
+        echo 'Artifactory server is not up'
     }
+
+    echo 'Artifactory server is up'
 }
 
 class JfrogBase {
@@ -35,16 +28,14 @@ class JfrogBase {
 
     public String repo
     public String reportsStorePath
-    def server = null
 
     // constructor
-    JfrogBase(String id, String serverUrl, String repo, String credentialsId, String reportsStorePath, def server) {
+    JfrogBase(String id, String serverUrl, String repo, String credentialsId, String reportsStorePath) {
         this.id = id
         this.serverUrl = serverUrl
         this.repo = repo
         this.credentialsId = credentialsId
         this.reportsStorePath = reportsStorePath
-        this.server = server
     }
 }
 
