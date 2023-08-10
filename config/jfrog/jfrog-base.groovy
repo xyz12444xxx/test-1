2a263d4eb7c4db95c3dbd80313909be9904e85eb
Kathim Anangoor-EXTERNAL Shereif <S0134951@gemalto.com>
2023-08-10 18:19:11 +0800

    Test-34

--- config/jfrog/jfrog-base.groovy
+++ config/jfrog/jfrog-base.groovy
@@ -52,8 +52,7 @@ private boolean copyAndZipFiles(String[] filenames, String fromDir, String zipFi
             sh "ls -l ${tempDir}"
         }
 
-        zip zipFile: "${zipFilename}", archive: false, dir: "${tempDir}"
-
+        zip zipFile: "${zipFilename}", archive: false, dir: "${tempDir}/*"
 
         // delete the temporary folder
         sh "rm -rf ${tempDir}"
