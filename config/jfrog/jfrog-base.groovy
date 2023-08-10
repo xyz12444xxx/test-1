011484bf8aa819cbe5c8f94c0e0d0c24f531e448
Kathim Anangoor-EXTERNAL Shereif <S0134951@gemalto.com>
2023-08-10 16:09:08 +0800

    Test-9

--- config/jfrog/jfrog-base.groovy
+++ config/jfrog/jfrog-base.groovy
@@ -44,14 +44,14 @@ def uploadReports(String fromDir, String[] filenames) {
             }
             echo "${filepaths}"
         }
-        if (!copyAndZipFiles(filepaths, fromDir, "reports.zip")) {
+        if (!copyAndZipFiles(filepaths, "temp", "reports.zip")) {
             echo "Failed to zip files"
             return
         }
 
         // def result = sh(script: "curl -XPUT -k -T ${fromDir}/${filename} ${this.serverUrl}/${this.repo}/${this.reportsStorePath}/${filename} -u " + '$USERNAME:$PASSWORD', returnStdout: true).trim()
         // pass the zipped file and show upload speed progress bar
-        def result = sh(script: "curl -XPUT --progress-bar -k -T reports.zip ${this.serverUrl}/${this.repo}/${this.reportsStorePath}/reports.zip -u " + '$USERNAME:$PASSWORD', returnStdout: true).trim()
+        def result = sh(script: "curl -XPUT --progress-bar -k -T temp/reports.zip ${this.serverUrl}/${this.repo}/${this.reportsStorePath}/reports.zip -u " + '$USERNAME:$PASSWORD', returnStdout: true).trim()
         echo "${result}"
     }
 }
