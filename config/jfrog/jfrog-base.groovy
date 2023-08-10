2d825125545ca9a4e90939d3ccbd4dcc9e8ec0b8
Kathim Anangoor-EXTERNAL Shereif <S0134951@gemalto.com>
2023-08-10 18:54:47 +0800

    Test-44

--- config/jfrog/jfrog-base.groovy
+++ config/jfrog/jfrog-base.groovy
@@ -48,7 +48,9 @@ private boolean copyAndZipFiles(String[] filenames, String fromDir, String zipFi
             sh "cp ${fromDir}/${filename} ${zipFilename}_temp"
         }
         
-        zip zipFile: "/${zipFilename}.zip", archive: false, dir: "${zipFilename}_temp", overwrite: true
+        dir {
+            zip zipFile: "/${zipFilename}.zip", archive: false, dir: "${zipFilename}_temp", overwrite: true
+        }
         sh "ls -l"
         sh "ls -l ${zipFilename}_temp"
     } catch (Exception e) {
